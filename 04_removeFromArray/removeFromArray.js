22c59e3a2b3d5ddb5a418669122b3a9558819b9e
Mitja Krašovec <mitjakrasovec1@gmail.com>
2022-05-25 10:07:45 +0200

    Finish exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,10 @@
-const removeFromArray = function(array, item, secondItem) {
-    secondItem = secondItem ?? 0;
-
-    if(secondItem === 0) {
-        const getIndex = array.indexOf(item);
-        array.splice(getIndex, 1);
-    } else {
-        const getFirstIndex = array.indexOf(item);
-        const getSecondIndex = array.indexOf(secondItem);
-
-        array.splice(getFirstIndex, 1) && array.splice(getSecondIndex, 1);
+const removeFromArray = function(array, ...items) {
+    items.forEach((item) => {
+        if(array.includes(item)){
+                array.splice(array.indexOf(item), 1);
     }
-
+    })
+    
     return array;
 };
 
