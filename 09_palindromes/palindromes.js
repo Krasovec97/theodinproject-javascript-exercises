cef1453c4b7701bef8feb67239f68a3c4f0a46e0
Mitja Krašovec <mitjakrasovec1@gmail.com>
2022-06-06 14:05:41 +0200

    Solved ecercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const CharArray = string.replace(/\W|_/g, '').toLowerCase().split('');
+    const reverseCharArray = CharArray.slice().reverse();
+    
+    const reverseString = reverseCharArray.join('');
+    const pureString = CharArray.join('');
 
+    return pureString === reverseString ? true : false;
+
+    
 };
 
 // Do not edit below this line
