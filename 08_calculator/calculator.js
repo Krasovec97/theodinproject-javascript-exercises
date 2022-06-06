9655d55e23e9af34905a87b44869cc7d3084ae5e
Mitja Krašovec <mitjakrasovec1@gmail.com>
2022-06-06 13:24:19 +0200

    Solved exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = (num1, num2) => {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = (num1, num2) => {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = (numArray) => {
+  
+	return numArray.length > 0 ? numArray.reduce((prevValue, curValue) => prevValue + curValue) : 0;
 };
 
-const multiply = function() {
-
+const multiply = (numArray2) => {
+  return numArray2.reduce((previous, next) => previous * next);
 };
 
-const power = function() {
-	
+const power = (num1, num2) => {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = (num) => {
+  var result = num;
+  if (num === 0 || num === 1) {
+    return 1; 
+  }
+
+  while (num > 1) { 
+    num--;
+    result *= num;
+  }
+  
+  return result;
 };
 
 // Do not edit below this line
