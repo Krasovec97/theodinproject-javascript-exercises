1b7b3e31ef0d7e60d0952e9c8478571f8626fcb7
Mitja Krašovec <mitjakrasovec1@gmail.com>
2022-06-06 15:59:48 +0200

    Solved Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    if(num < 0) {
+        return "OOPS";
+    }
 
-};
+    if(+num === 1) {
+        return 1;
+    }
+
+    const convertedNumber = +num;
+    let numArray = Array.from({length: convertedNumber}, (value, index) => index);
+
+    const fibArray = numArray.reduce((prevValue, currentValue, curIndex) => {
+        prevValue.push((curIndex <= 1) ? curIndex : prevValue[curIndex-2] + prevValue[curIndex-1])
+        return prevValue;
+    }, [])
+
+
+    return fibArray[fibArray.length - 1] + fibArray[fibArray.length - 2];
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
