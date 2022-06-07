39329f25f97487d58f26861d6f7b2116e74d65da
Mitja Krašovec <mitjakrasovec1@gmail.com>
2022-06-07 11:52:58 +0200

    Finished Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+        return people.reduce((prevValue, currentValue) => {
+            if(prevValue.hasOwnProperty('yearOfDeath') && currentValue.hasOwnProperty('yearOfDeath')){
+                const getPrevYears = prevValue.yearOfDeath - prevValue.yearOfBirth;
+                const getCurrentYears = currentValue.yearOfDeath - currentValue.yearOfBirth
+                return getPrevYears > getCurrentYears ? prevValue : currentValue;
+            } else {
+                const today = new Date().getFullYear();
+                const getPrevDateOfBirth = today - prevValue.yearOfBirth;
+                const getCurrentDateOfBirth = today - currentValue.yearOfBirth;
+                return getCurrentDateOfBirth > getPrevDateOfBirth ? currentValue : prevValue;
+            }
+
+        })
 };
 
 // Do not edit below this line
